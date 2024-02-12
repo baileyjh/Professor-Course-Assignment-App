bee5b3b7603d7847526e5e5cdaa2a3b01ea2d5a3
Bailey Hall <baileyhall@Baileys-MacBook-Pro.local>
2024-02-12 11:33:43 -0800

    Why are there 20 changes?

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import ProfessorList from './components/ProfessorList';
+import { Professor } from './model';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const App: React.FC = () => {
+  const [professor, setProfessor] = useState<string>("");
+  const [professors, setProfessors]= useState<Professor[]>([]);
+  const [assignedCourse, setAssignedCourse] = useState<Professor[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(professor) {
+      setProfessors([...professors, {id: Date.now(), professor:professor, isDone: false}])
+      setProfessor("");
+    }
+  };
+
+  console.log(professors);
+
+  return (
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="App">
+      <span className="heading"> Professor-Course Assignment Tool</span>
+      <InputField professor={professor} setProfessor={setProfessor} handleAdd={handleAdd} />
+      <ProfessorList 
+        professors={professors} 
+        setProfessors={setProfessors}
+        assignedCourse={assignedCourse} 
+        setAssignedCourse={setAssignedCourse}/> 
+    </div>
+    </DragDropContext>
+
+  );
+}
+
+export default App;
