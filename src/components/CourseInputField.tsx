3ee1ffc76b4a2be8a648b4857db88633abcbfedd
Bailey Hall <baileyhall@Baileys-MacBook-Pro.local>
2024-03-17 13:16:01 -0700

    focus on input boxes stay until click out

--- src/components/CourseInputField.tsx
+++ src/components/CourseInputField.tsx
@@ -9,18 +9,20 @@ interface Props{
 
 const CourseInputField: React.FC<Props> = ({ course, setCourse, handleAdd }) => {
     const inputRef = useRef<HTMLInputElement>(null);
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault(); 
+        handleAdd(e);
+        if (inputRef.current) {
+            inputRef.current.focus(); 
+        }
+    };
     return (
-        <form className="input" onSubmit={(e)=>{
-            handleAdd(e)
-            inputRef.current?.blur();
-            }}>
+        <form className="input" onSubmit={handleSubmit}>
             <input 
                 ref={inputRef}
                 type="input" 
                 value={course}
-                onChange={
-                    (e)=>setCourse(e.target.value)
-                }
+                onChange={(e)=>setCourse(e.target.value)}
                 placeholder = "Enter a course" 
                 className="input_box"/>
             <button className="input_submit_course" type="submit">Add</button>
--- src/components/ProfessorInputField.tsx
+++ src/components/ProfessorInputField.tsx
@@ -9,20 +9,22 @@ interface Props{
 
 const ProfessorInputField: React.FC<Props> = ({ professor, setProfessor, handleAdd }) => {
     const inputRef = useRef<HTMLInputElement>(null);
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault(); 
+        handleAdd(e);
+        if (inputRef.current) {
+            inputRef.current.focus(); 
+        }
+    };
     return (
-        <form className="input" onSubmit={(e)=>{
-            handleAdd(e)
-            inputRef.current?.blur();
-            }}>
-            <input 
+        <form className="input" onSubmit={handleSubmit}>
+            <input
                 ref={inputRef}
-                type="input" 
+                type="input"
                 value={professor}
-                onChange={
-                    (e)=>setProfessor(e.target.value)
-                }
-                placeholder = "Enter a professor" 
-                className="input_box"/>
+                onChange={(e) => setProfessor(e.target.value)}
+                placeholder="Enter a professor"
+                className="input_box" />
             <button className="input_submit_prof" type="submit">Add</button>
         </form>
     );
